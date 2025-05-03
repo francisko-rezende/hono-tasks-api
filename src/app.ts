21dd520f6ad6bb7c7946766e8211617fb67c9650
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-05-03 15:30:43 -0300

    feat: setup middlewares for untreated errors and not found routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
+import { notFound } from './middleware/not-found.js'
+import onError from './middleware/on-error.js'
 
 export const app = new OpenAPIHono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.get('/error', (c) => {
+  throw new Error('errou!')
+})
+
+app.notFound(notFound)
+app.onError(onError)
