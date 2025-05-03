f90f467774460fed3039ca4a6e0e217aee5ccd66
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-05-03 20:27:28 -0300

    feat: setup scalar for endpoint testing

--- src/lib/configure-open-api.ts
+++ src/lib/configure-open-api.ts
@@ -1,12 +1,24 @@
 import type { AppOpenAPI } from './types.js'
 import packageJSON from '../../package.json'
+import { Scalar } from '@scalar/hono-api-reference'
 
 export function configureOpenAPI(app: AppOpenAPI) {
-  app.doc('/doc', {
+  const docURL = '/doc'
+  app.doc(docURL, {
     openapi: '3.0.0',
     info: {
       version: packageJSON.version,
       title: 'Tasks API',
     },
   })
+
+  app.get(
+    '/scalar',
+    Scalar({
+      layout: 'classic',
+      defaultHttpClient: { targetKey: 'js', clientKey: 'fetch' },
+      theme: 'purple',
+      url: docURL,
+    }),
+  )
 }
