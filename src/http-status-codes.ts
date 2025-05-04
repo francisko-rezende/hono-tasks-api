7f766959afb7b0b4f3de19547790def3245fd321
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-05-03 21:04:51 -0300

    fix: cast http-status as const so it satisfies ts constraints when using its contents

--- src/http-status-codes.ts
+++ src/http-status-codes.ts
@@ -353,4 +353,4 @@ export const httpStatusCodes = {
    * Defined in the specification of HTTP/2 to indicate that a server is not able to produce a response for the combination of scheme and authority that are included in the request URI.
    */
   MISDIRECTED_REQUEST: 421,
-}
+} as const
