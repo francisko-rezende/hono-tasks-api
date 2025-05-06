e91cb126b2a19807d84b0b25ba44b11dd6477725
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-05-06 10:17:20 -0300

    feat: implement seed for tasks

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import { env } from '@/env'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { tasksTable } from './schema'
+import { reset, seed } from 'drizzle-seed'
 
 const db = drizzle({
   connection: {
@@ -9,16 +10,9 @@ const db = drizzle({
 })
 
 async function main() {
-  const task: typeof tasksTable.$inferInsert = {
-    name: 'First seed task',
-    done: false,
-  }
-
-  await db.insert(tasksTable).values(task)
-  console.log('New task created')
-
-  const tasks = await db.select().from(tasksTable)
-  console.log(`Getting all tasks from db:`, tasks)
+  await reset(db, { tasksTable })
+  await seed(db, { tasksTable })
+  console.log('db seeded!')
 }
 
 main()
