9f66233d0ad3b64c5ffb622028e59464fd3ae347
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-05-06 10:40:57 -0300

    fix: use mode date to get tz aware values from created at and updated at columns

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,8 +6,8 @@ export const tasksTable = pgTable('tasks', {
   id: uuid().defaultRandom().primaryKey(),
   name: varchar({ length: 255 }).notNull(),
   done: boolean().notNull(),
-  createdAt: timestamp('created_at', { mode: 'string' }).notNull().defaultNow(),
-  updatedAt: timestamp('updated_at', { mode: 'string' })
+  createdAt: timestamp('created_at', { mode: 'date' }).notNull().defaultNow(),
+  updatedAt: timestamp('updated_at', { mode: 'date' })
     .notNull()
     .defaultNow()
     .$onUpdateFn(() => sql`now()`),
