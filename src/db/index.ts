b3c2c7a3bdb81115996ad3106799de67dbb73f93
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-05-06 10:35:45 -0300

    feat: start using drizzle to return tasks from db

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,11 +2,13 @@ import { env } from '@/env'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { tasksTable } from './schema'
 import { reset, seed } from 'drizzle-seed'
+import * as schema from './schema'
 
-const db = drizzle({
+export const db = drizzle({
   connection: {
     connectionString: env.DATABASE_URL,
   },
+  schema,
 })
 
 async function main() {
--- src/routes/tasks/tasks.handlers.ts
+++ src/routes/tasks/tasks.handlers.ts
@@ -1,13 +1,10 @@
 import type { AppRouterHandler } from '@/lib/types'
 import type { ListTasksRoute } from './tasks.routes'
+import { db } from '@/db'
 
-const list: AppRouterHandler<ListTasksRoute> = (c) => {
-  return c.json([
-    {
-      name: 'Learn Hono',
-      done: false,
-    },
-  ])
+const list: AppRouterHandler<ListTasksRoute> = async (c) => {
+  const tasks = await db.query.tasksTable.findMany()
+  return c.json(tasks)
 }
 
 export const tasksHandlers = {
